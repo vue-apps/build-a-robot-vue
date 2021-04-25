a631c5cd270e7a5d3920482514b8385c03b7c4f6
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-04-26 02:29:59 +0500

    clean up, create HomePage component and styling

---
+++ src/components/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <div>
+      <img src="../assets/robot-home.png" class="robot" aria-hidden="true" />
+    </div>
+    <div class="get-started">
+      <a href="#">Get Started</a> building your first robot!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.home {
+  text-align: center;
+
+  .robot {
+    height: 300px;
+  }
+}
+</style>
