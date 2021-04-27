48ad505337c52cf568a79d390bba77ee01db9130
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-04-27 20:30:18 +0500

    call emitSelected method in both created() & updated() lifecycle hooks

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -40,6 +40,9 @@ export default {
   created() {
     this.emitSelectedPart();
   },
+  updated() {
+    this.emitSelectedPart();
+  },
 
   methods: {
     emitSelectedPart() {
@@ -51,14 +54,12 @@ export default {
         this.selectedPartIndex,
         this.parts.length
       );
-      this.emitSelectedPart();
     },
     selectPreviousPart() {
       this.selectedPartIndex = getPreviousValidIndex(
         this.selectedPartIndex,
         this.parts.length
       );
-      this.emitSelectedPart();
     },
   },
 };
