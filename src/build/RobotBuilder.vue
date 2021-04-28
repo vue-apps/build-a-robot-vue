8f1b32cc00feea4b34dbe40164d18285cf449328
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-04-28 23:43:49 +0500

    use route beforeRouteLeave() guard to prevent a user from leaving a page in RobotBuilder component

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -87,6 +87,16 @@ import CollapsibleSection from '@/shared/CollapsibleSection';
 
 export default {
   name: 'RobotBuilder',
+  beforeRouteLeave(to, from, next) {
+    if (this.addedToCart) {
+      next(true);
+    } else {
+      const response = confirm(
+        'You have not added your robot to your cart, are you sure want to leave? '
+      );
+      next(response);
+    }
+  },
   components: {
     PartSelector,
     CollapsibleSection,
@@ -94,6 +104,7 @@ export default {
   data() {
     return {
       availableParts,
+      addedToCart: false,
       cart: [],
       selectedRobot: {
         head: {},
@@ -120,7 +131,8 @@ export default {
         robot.torso.cost +
         robot.base.cost;
       this.cart.push(Object.assign({}, robot, { cost }));
-      console.log(this.cart);
+      this.addedToCart = true;
+      // console.log(this.cart);
     },
   },
 };
