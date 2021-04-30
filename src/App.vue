407b13378daa080bf3ac31d683f0b07da5b6e947
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-04-30 18:17:43 +0500

    organize the store with modules, create users module

--- src/App.vue
+++ src/App.vue
@@ -46,7 +46,7 @@ export default {
   name: 'App',
   computed: {
     cart() {
-      return this.$store.state.cart;
+      return this.$store.state.robots.cart;
     },
   },
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,39 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import axios from 'axios';
+import RobotsModule from '@/store/modules/robots';
+import usersModule from '@/store/modules/users';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    cart: [],
-    parts: null,
-  },
-  getters: {
-    cartSaleItems(state) {
-      return state.cart.filter((item) => item.head.onSale);
-    },
-  },
-  actions: {
-    getParts({ commit }) {
-      axios
-        .get('/api/parts')
-        .then((result) => commit('updateParts', result.data))
-        .catch(console.error);
-    },
-    addRobotToCart({ commit, state }, robot) {
-      const cart = [...state.cart, robot];
-      return axios
-        .post('/api/cart', cart)
-        .then(() => commit('addRobotToCart', robot));
-    },
-  },
-  mutations: {
-    addRobotToCart(state, robot) {
-      state.cart.push(robot);
-    },
-    updateParts(state, parts) {
-      state.parts = parts;
-    },
+  modules: {
+    robots: RobotsModule,
+    users: usersModule,
   },
 });
