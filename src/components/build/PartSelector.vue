bdce4d7dfb63362347826fbe739d60617b47b6b6
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-05-07 16:30:53 +0500

    use update lifecycle hook to update custom directive

--- src/components/build/PartSelector.vue
+++ src/components/build/PartSelector.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="part" :class="position">
+    {{ pinPadding }}
     <img @click="showPartInfo" :src="selectedPart.src" title="arm" />
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
     <span
-      v-pin="{ top: '10px', right: '5px' }"
+      @click="pinPadding = '30px'"
+      v-pin="{ top: pinPadding, right: pinPadding }"
       class="sale"
       v-show="selectedPart.onSale"
       >Sale!</span
@@ -37,7 +39,7 @@ export default {
     },
   },
   data() {
-    return { selectedPartIndex: 0 };
+    return { selectedPartIndex: 0, pinPadding: '10px' };
   },
   computed: {
     selectedPart() {
