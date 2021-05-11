9e1a258d406f6d14b3b039df5c0ab57523a7747c
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-05-11 20:00:10 +0500

    add custom webpack config for testing purposes

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.coffee$/,
+          use: ['coffee-loader'],
+        },
+      ],
+    },
+  },
   devServer: {
     proxy: {
       '/api': {
